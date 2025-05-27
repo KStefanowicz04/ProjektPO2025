767ce189c57489f93276dc5f925dff349b800848
Tomasz <atomekjan@gmail.com>
2025-05-27 15:44:20 +0200

    stworzenie 3 plikow .cs

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test1.PowiedzCzesc();
+            Test2.PowiedzHej();
+        }
+    }
---
+++ Stefanowicz.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Test1
+{
+    public static void PowiedzCzesc()
+    {
+        Console.WriteLine("Cześć z Plik1!");
+    }
+}
