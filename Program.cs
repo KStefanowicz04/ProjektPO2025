4681829ac7e4813c20502e05ecf0aa390e81e595
KStefanowicz04 <kacper.stefanowicz04@gmail.com>
2025-06-08 18:16:12 +0200

    Poprawa UML, dodano Bike i pochodne

--- Program.cs
+++ Program.cs
@@ -1,10 +1,57 @@
 ﻿using System;
 
-    class Program
+class Program
+{
+    // Klasa abstrakcyjna Bike
+    abstract class Bike
     {
-        static void Main(string[] args)
+        // Pola
+        int id;
+        public int ID  // getter
         {
-            Test1.PowiedzCzesc();
-            Test2.PowiedzHej();
+            get { return id; }
+        }
+        double price;
+        public double Price  // getter
+        {
+            get { return price; }
+        }
+
+        // Konstruktor
+        public Bike(double price)
+        {
+            // Losowanie ID roweru
+
+            this.price = price;
         }
     }
+
+    // Rower zwykły
+    public class RegularBike : Bike
+    {
+        // Konstruktor
+        public RegularBike(double price) : base(price) { }
+    }
+
+    // Rower dziecięcy
+    public class ChildBike : Bike
+    {
+        // Konstruktor
+        public RegularBike(double price) : base(price) { }
+    }
+
+    // Rower tandem
+    public class TandemBike : Bike
+    {
+        // Konstruktor
+        public RegularBike(double price) : base(price) { }
+    }
+
+
+    // Funkcja main
+    static void Main(string[] args)
+    {
+        Test1.PowiedzCzesc();
+        Test2.PowiedzHej();
+    }
+}
